41e036c01b46f9793a1cb289dbb3c501d6c5f27f
Tetsuya Mori <monry84@gmail.com>
2019-06-13 23:18:22 +0900

    :recycle: Refactor command runner

---
+++ scripts/lib/runner.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+case "${COMMAND}" in
+  'init' )
+    source "${DIRECTORY}/scripts/upm-init.sh" "${PARAM[@]}"
+    ;;
+  'add' )
+    case "${SUBCOMMAND}" in
+      'registry' )
+        source "${DIRECTORY}/scripts/upm-add-registry.sh" "${PARAM[@]}"
+        ;;
+      'package' )
+        source "${DIRECTORY}/scripts/upm-add-package.sh" "${PARAM[@]}"
+        ;;
+    esac
+    ;;
+esac
