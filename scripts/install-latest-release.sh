8ea4b3ea9fcd61f31db69a7fe915a14c8f4e3046
Tetsuya Mori <monry84@gmail.com>
2019-06-13 15:59:28 +0900

    :wrench: Tweak version detection

--- scripts/install-latest-release.sh
+++ scripts/install-latest-release.sh
@@ -2,7 +2,7 @@
 
 set -ue
 
-latest_version=$( git ls-remote --tags https://github.com/upm-packages/upm-cli.git | sed -nE 's#.*refs/tags/(v?[0-9]+(\.[0-9]+)*)$#\1#p' | sort -Vr | head -n 1 )
+latest_version=$( git ls-remote --tags https://github.com/upm-packages/upm-cli.git | sed -nE 's#.*refs/tags/(v?[0-9]+(\.[0-9]+)*(-(alpha|beta|preview)\.?[0-9]*)?)$#\1#p' | sort -Vr | head -n 1 )
 name="upm-cli-${latest_version//v/}"
 tarball_file="${name}.tar.gz"
 curl -sLo ${tarball_file} https://github.com/upm-packages/upm-cli/archive/${latest_version}.tar.gz
