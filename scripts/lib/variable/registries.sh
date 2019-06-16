79a532d596acb6cc918af5f68835d7c6c6b5fea0
Tetsuya Mori <monry84@gmail.com>
2019-06-16 22:40:36 +0900

    :beetle: Support no registries configured

--- scripts/lib/variable/registries.sh
+++ scripts/lib/variable/registries.sh
@@ -4,10 +4,12 @@ REGISTRIES=()
 registry_list=""
 registry_names_json=$(cat ${config_file} | jq '.registries|keys')
 length=$(echo ${registry_names_json} | jq 'length')
-for index in $( seq 0 $(($length - 1)) )
-do
-  registry_name=$(echo ${registry_names_json} | jq -r ".[${index}]")
-  REGISTRIES+=(${registry_name})
-  registry_list="${registry_list}[${index}] ${registry_name}
+if [ $length -ge 1 ]; then
+  for index in $( seq 0 $(($length - 1)) )
+  do
+    registry_name=$(echo ${registry_names_json} | jq -r ".[${index}]")
+    REGISTRIES+=(${registry_name})
+    registry_list="${registry_list}[${index}] ${registry_name}
 "
-done
+  done
+fi
